5f8b3e14e61a692d23f39a27386383caed5feced
Hollow Knight <njnikhileshwaran@gmail.com>
2025-12-13 02:07:36 +0530

    Initial

---
+++ app/calls.ts
@@ -0,0 +1,19 @@
+const counterContractAddress = '0xacbeDDa8afCC1E4E0CBA3fDf4e6A371cE037F1E3'; // add your contract address here
+const counterContractAbi = [
+  {
+    type: 'function',
+    name: 'increment',
+    inputs: [],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+] as const;
+
+export const calls = [
+  {
+    address: counterContractAddress,
+    abi: counterContractAbi,
+    functionName: 'increment',
+    args: [],
+  },
+];
