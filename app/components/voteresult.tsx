4eceffd2013a5629f8df3504aec640a8bf4eb98e
nikillxh <njnikhilesh17@gmail.com>
2025-12-19 15:40:14 +0530

    Final beta

---
+++ app/components/voteresult.tsx
@@ -0,0 +1,16 @@
+import { useLeadingCandidate } from "@/calls"
+
+const candidateNames = ["Hoodie Alpha", "Hoodie Beta", "Hoodie Gamma", "Hoodie Delta", "Hoodie Sigma"];
+
+export default function VoteResult(voteProps: {qvEnded?: boolean}) {
+    var lead = useLeadingCandidate();
+    if (lead === null) return <pre>Trump has the most votes lol</pre>;
+
+    return (
+        voteProps.qvEnded ? (
+        <pre>The winner is {candidateNames[Number(lead)]} ᯓ★</pre>
+        ) : (
+        <pre>{candidateNames[Number(lead)]} is taking the lead 𓐬</pre>
+    )
+    )
+}
