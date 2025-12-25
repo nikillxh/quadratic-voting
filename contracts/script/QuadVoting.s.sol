3844026eecb68a7c4a49efcc483430c7c3868725
PARTH ARVIND RATHI <cs5240875@iitd.ac.in>
2025-12-25 11:12:59 +0530

    script

--- contracts/script/QuadVoting.s.sol
+++ contracts/script/QuadVoting.s.sol
@@ -45,28 +45,30 @@ contract DeployQuadVoting is Script {
 
 contract SetupVoters is Script {
     function run() external {
-        // Load contract address from environment or argument
         address contractAddress = vm.envAddress("CONTRACT_ADDRESS");
         uint256 overseerPrivateKey = vm.envUint("PRIVATE_KEY");
         
         QuadVoting quadVoting = QuadVoting(contractAddress);
         
-        // Prepare voter addresses
-        address[] memory voters = new address[](5);
+        address[] memory voters = new address[](10);
         voters[0] = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
         voters[1] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
         voters[2] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
         voters[3] = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
         voters[4] = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
+        voters[5] = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
+        voters[6] = 0x976EA74026E726554dB657fA54763abd0C3a0aa9;
+        voters[7] = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955;
+        voters[8] = 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f;
+        voters[9] = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
         
         vm.startBroadcast(overseerPrivateKey);
         
-        // Give vote access
         quadVoting.giveVoteAccess(voters);
         
         vm.stopBroadcast();
         
-        console.log("Vote access granted to", voters.length, "addresses");
+        console.log("Vote access granted on Sepolia to", voters.length, "addresses");
     }
 }
 
